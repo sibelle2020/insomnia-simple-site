993a35961e51af124747c916da4fb116d5531e14
Sandra Sibelle <sibellemusic@gmail.com>
2021-04-11 20:06:14 +0200

    added js and css to submit form

--- js/post-suggestion.js
+++ js/post-suggestion.js
@@ -1,2 +1,11 @@
 const form = document.querySelector(".submit-form");
-console.log(form.elements.title);
+
+form.elements.title.addEventListener("keyup", (e) => {
+  if (e.target.value.length > 25) {
+    form.elements.username.focus();
+  }
+});
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+});
