3fd533e2afaa90ff8780124c5381a6094fa730ee
Sandra Sibelle <sibellemusic@gmail.com>
2021-04-11 20:37:26 +0200

    added js to form

--- js/post-suggestion.js
+++ js/post-suggestion.js
@@ -8,4 +8,28 @@ form.elements.title.addEventListener("keyup", (e) => {
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
+
+  const payload = {
+      title: form.elements.title.value,
+      username: form.elements.username.value,
+      content: form.elements.content.value;
+  }
+
+  document.querySelector("input[type=submit]").disabled = true;
+
+  fetch("https://s21kea-d06b.restdb.io/rest/posts", {
+    method: "POST",
+    headers: {
+      "x-apikey": "6033bd605ad3610fb5bb64f6",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(payload), 
+      
+  })
+    .then((response) => {
+      console.log(response);
+    })
+    .catch((err) => {
+      console.error(err);
+    });
 });
